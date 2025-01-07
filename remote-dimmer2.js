7113b5c50013cf0a7fe40cbb9c6ac8abe74e6f91
Simon Lehmann <simon.lmn@gmail.com>
2025-01-07 12:44:46 +0100

    Add HTTP basic authentication and reorder code

--- remote-dimmer2.js
+++ remote-dimmer2.js
@@ -1,81 +1,33 @@
+// Possible actions for switching the light
 const LIGHT_ACTIONS = {
     ON: "on",
     OFF: "off",
     TOGGLE: "toggle"
 }
 
-// Constants for dimmer actions
+// Possible actions for dimming the light
 const DIMMER_ACTIONS = {
     STOP: "stop",
     UP: "up",
     DOWN: "down"
 };
 
-// Constants for state machine states
-const STATES = {
-    IDLE: "IDLE",
-    GET_LIGHT_STATUS: "GET_LIGHT_STATUS",
-    WAITING_FOR_LONG_PUSH: "WAITING_FOR_LONG_PUSH",
-    ENSURE_LIGHT_IS_ON: "ENSURE_LIGHT_IS_ON",
-    DIMMING: "DIMMING"
-};
-
-// Valid state transitions
-const TRANSITIONS = {
-    IDLE: [STATES.GET_LIGHT_STATUS, STATES.IDLE],
-    GET_LIGHT_STATUS: [STATES.WAITING_FOR_LONG_PUSH, STATES.IDLE],
-    WAITING_FOR_LONG_PUSH: [STATES.ENSURE_LIGHT_IS_ON, STATES.IDLE],
-    ENSURE_LIGHT_IS_ON: [STATES.DIMMING, STATES.IDLE],
-    DIMMING: [STATES.DIMMING, STATES.IDLE]
-}
-
 // Configuration settings for the dimmer device
 const CONFIG = {
-    dimmerDeviceAddress: "192.168.3.127",
+    dimmerDevice: {
+        address: "192.168.3.127", // IP address of the dimmer device
+        auth: null, // can be "username:password" or null for no authentication
+    },
     inputMapping: { "input:0": DIMMER_ACTIONS.DOWN, "input:1": DIMMER_ACTIONS.UP },
     longPushTime: 500
 };
 
 // Data object to hold the current state and other relevant information
 let DATA = {
-    state: STATES.IDLE,
+    state: null,
     inputTriggerTime: 0,
     inputAction: DIMMER_ACTIONS.STOP,
     lightIsOn: false
-}
-
-/**
- * Transition to a new state and execute the corresponding function.
- * @param {string} state - The new state to transition to.
- */
-function transitionTo(state) {
-    if (TRANSITIONS[DATA.state].indexOf(state) === -1) {
-        return;
-    }
-
-    print("Transitioning from " + DATA.state + " to " + state);
-
-    DATA.state = state;
-    switch (state) {
-        case STATES.GET_LIGHT_STATUS:
-            enterGetLightStatus();
-            break;
-        case STATES.WAITING_FOR_LONG_PUSH:
-            enterWaitingForLongPush();
-            break;
-        case STATES.ENSURE_LIGHT_IS_ON:
-            enterEnsureLightIsOn();
-            break;
-        case STATES.DIMMING:
-            enterDimming();
-            break;
-        case STATES.IDLE:
-            enterIdle();
-            break;
-        default:
-            print("Unknown state", state);
-            break;
-    }
 }
 
 /**
@@ -84,7 +36,7 @@ function transitionTo(state) {
  * @param {function} callback - The callback function to execute when the request completes.
  */
 function sendRequest(endpointPath, callback) {
-    Shelly.call("http.get", { url: "http://" + CONFIG.dimmerDeviceAddress + "/" + endpointPath }, callback, null);
+    Shelly.call("http.get", { url: "http://" + (CONFIG.dimmerDevice.auth ? (CONFIG.dimmerDevice.auth + "@") : "") + CONFIG.dimmerDevice.address + "/" + endpointPath }, callback, null);
 }
 
 /**
@@ -137,35 +89,75 @@ function dimLight(action, callback) {
     });
 }
 
+// Constants for state machine states
+const STATES = {
+    IDLE: "IDLE",
+    GET_LIGHT_STATUS: "GET_LIGHT_STATUS",
+    WAITING_FOR_LONG_PUSH: "WAITING_FOR_LONG_PUSH",
+    ENSURE_LIGHT_IS_ON: "ENSURE_LIGHT_IS_ON",
+    DIMMING: "DIMMING"
+};
+
+// Valid state transitions
+const TRANSITIONS = {
+    IDLE: [STATES.GET_LIGHT_STATUS, STATES.IDLE],
+    GET_LIGHT_STATUS: [STATES.WAITING_FOR_LONG_PUSH, STATES.IDLE],
+    WAITING_FOR_LONG_PUSH: [STATES.ENSURE_LIGHT_IS_ON, STATES.IDLE],
+    ENSURE_LIGHT_IS_ON: [STATES.DIMMING, STATES.IDLE],
+    DIMMING: [STATES.DIMMING, STATES.IDLE]
+}
+
 /**
- * Enter the idle state, clearing any timers and resetting relevant data.
+ * Transition to a new state and execute the corresponding function.
+ * @param {string} state - The new state to transition to.
  */
+function transitionTo(state) {
+    if (DATA.state && TRANSITIONS[DATA.state].indexOf(state) === -1) {
+        return;
+    }
+
+    print("Transitioning from " + DATA.state + " to " + state);
+
+    DATA.state = state;
+    switch (state) {
+        case STATES.GET_LIGHT_STATUS:
+            enterGetLightStatus();
+            break;
+        case STATES.WAITING_FOR_LONG_PUSH:
+            enterWaitingForLongPush();
+            break;
+        case STATES.ENSURE_LIGHT_IS_ON:
+            enterEnsureLightIsOn();
+            break;
+        case STATES.DIMMING:
+            enterDimming();
+            break;
+        case STATES.IDLE:
+            enterIdle();
+            break;
+        default:
+            print("Unknown state", state);
+            break;
+    }
+}
+
 function enterIdle() {
     DATA.inputTriggerTime = 0;
     DATA.inputAction = DIMMER_ACTIONS.STOP;
 }
 
-/**
- * Enter the state to get the light status.
- */
 function enterGetLightStatus() {
     getLightStatus(function () {
         transitionTo(STATES.WAITING_FOR_LONG_PUSH);
     });
 }
 
-/**
- * Enter the state to wait for a long push.
- */
 function enterWaitingForLongPush() {
     Timer.set(Math.max(1, CONFIG.longPushTime - Math.round(Date.now() - DATA.inputTriggerTime)), false, function () {
         transitionTo(STATES.ENSURE_LIGHT_IS_ON);
     }, null);
 }
 
-/**
- * Enter the state to ensure the light is on.
- */
 function enterEnsureLightIsOn() {
     if (!DATA.lightIsOn) {
         switchLight(LIGHT_ACTIONS.ON, function () {
@@ -176,18 +168,11 @@ function enterEnsureLightIsOn() {
     }
 }
 
-/**
- * Enter the dimming state, starting the dimming loop.
- */
 function enterDimming() {
     dimLight(DATA.inputAction, function () { });
 }
 
-/**
- * Handle status changes from the Shelly device.
- * @param {object} e - The event object containing the status change information.
- */
-Shelly.addStatusHandler(function (e) {
+function handleInputEvent(e) {
     if (CONFIG.inputMapping.hasOwnProperty(e.component)) {
         print("---> input event", e.component, e.delta.state);
         if (e.delta.state === true) {
@@ -212,4 +197,11 @@ Shelly.addStatusHandler(function (e) {
             }
         }
     }
-});
+}
+
+function main() {
+    transitionTo(STATES.IDLE);
+    Shelly.addStatusHandler(handleInputEvent);
+}
+
+main();
